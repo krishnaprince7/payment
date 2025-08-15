4437667b93f8eee3c55fe38e2227825554053a4f
krishna vishwakama <146770906+krishnaprince7@users.noreply.github.com>
2025-08-16 01:10:48 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,15 +4,12 @@ import cors from "cors"
 import cookieParser from "cookie-parser";
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(cors({
-   origin: process.env.FRONTEND_URL || "http://localhost:5173", 
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"], // Explicitly allow
-}));
-
-
-
+    origin: process.env.CORS_ORIGIN,
+    credentials : true
+}))
 
 
 app.use(express.json({limit : "16kb"}))

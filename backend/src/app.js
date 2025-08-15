f00602370102edb977dd0a73898b9c7fbeaf400c
krishna vishwakama <146770906+krishnaprince7@users.noreply.github.com>
2025-08-16 01:38:58 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.FRONTEND_URL,
     credentials : true
 }))
 
